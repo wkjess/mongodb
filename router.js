b9d9835a0710b02d0dbc9fc9e25cea9ddcd1e173
Jessie Wong <56956346+wkjess@users.noreply.github.com>
2020-03-26 10:46:31 +0000

    Create router.js

---
+++ router.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+
+router = express.Router(),
+
+itemCtrl = require('./item-controller');
+
+
+
+router.get('/hello/:foo/:bar', itemCtrl.getWorld);
+
+
+
+router.post('/hello', itemCtrl.postWorld);
+
+
+
+module.exports = router;
